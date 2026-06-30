d288082c1a047376e193feccca9c3beff5de81d3
Alokkumar Gupta <alokkumar568g@gmail.com>
2026-06-30 15:30:51 +0530

    Change axios base URL to production server

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://leetcode-clone-8sgq.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
