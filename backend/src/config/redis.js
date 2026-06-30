f3a6218cbc37388e994927a83ca2baf0f291e9c0
Alokkumar Gupta <alokkumar568g@gmail.com>
2026-06-30 15:21:57 +0530

    Update redis.js

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -2,10 +2,10 @@ const { createClient }  = require('redis');
 
 const redisClient = createClient({
     username: 'default',
-    password: process.env.REDIS_PASS,
-     socket: {
-       host: 'redis-15568.c246.us-east-1-4.ec2.cloud.redislabs.com',
-        port: 15568
+    password: '3Pved2y19w49hpmOvsFD6QHX7Q8VY91C',
+    socket: {
+        host: 'redis-12163.c246.us-east-1-4.ec2.cloud.redislabs.com',
+        port: 12163
     }
 });
 
