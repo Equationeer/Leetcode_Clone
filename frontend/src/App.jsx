0606ad36425bf1205a74116718d0d9fca6d65dc8
Alokkumar Gupta <alokkumar568g@gmail.com>
2026-07-03 00:25:51 +0530

    Add route for updating problems in admin panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,6 +36,7 @@ function App(){
       <Route path="/signup" element={isAuthenticated?<Navigate to="/" />:<Signup></Signup>}></Route>
       <Route path="/admin" element={isAuthenticated && user?.role === 'admin' ? <Admin /> : <Navigate to="/" />} />
       <Route path="/admin/create" element={isAuthenticated && user?.role === 'admin' ? <AdminPanel /> : <Navigate to="/" />} />
+      <Route path="/admin/update/:problemId" element={isAuthenticated && user?.role === "admin" ? <AdminPanel />: <Navigate to="/" />} />
       <Route path="/admin/delete" element={isAuthenticated && user?.role === 'admin' ? <AdminDelete /> : <Navigate to="/" />} />
       <Route path="/admin/video" element={isAuthenticated && user?.role === 'admin' ? <AdminVideo /> : <Navigate to="/" />} />
       <Route path="/admin/upload/:problemId" element={isAuthenticated && user?.role === 'admin' ? <AdminUpload /> : <Navigate to="/" />} />
